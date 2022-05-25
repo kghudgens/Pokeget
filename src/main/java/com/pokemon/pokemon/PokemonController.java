dec7c923c711a3103ff7ea1cdedc41c7fca0eabd
Kevin Hudgens <hudgens1073@gmail.com>
2022-05-25 09:30:22 +0900

    corrected the annotations

--- src/main/java/com/pokemon/pokemon/PokemonController.java
+++ src/main/java/com/pokemon/pokemon/PokemonController.java
@@ -1,12 +1,14 @@
 package com.pokemon.pokemon;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+// Need to use controller because thymeleaf does not support rest controller
+@Controller
 public class PokemonController {
 
-    @GetMapping("/")
+    @GetMapping
     public String index(){
         return "index";
     }
