a639f9e4faf6c3cf083e6a691f8a0d396f3069ff
Kevin Hudgens <hudgens1073@gmail.com>
2022-05-27 12:51:01 +0900

    added dependency

---
+++ src/test/java/com/pokemon/PokemonServiceTests.java
@@ -0,0 +1,24 @@
+package com.pokemon;
+
+import com.pokemon.pokemon.PokemonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class PokemonServiceTests {
+
+    @Autowired
+    private PokemonService pkService;
+
+    @Test
+    public void testGetPokemonPlainJSON(){
+        String obj = pkService.getPokemonPlainJSON();
+        System.out.println(obj);
+        assertFalse(obj.isEmpty());
+    }
+
+}
