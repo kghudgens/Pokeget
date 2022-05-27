5751038af2f5385209d02c9d66ee0f60ecfbebbc
Kevin Hudgens <hudgens1073@gmail.com>
2022-05-27 11:42:16 +0900

    created reference to the service

--- src/main/java/com/pokemon/pokemon/PokemonController.java
+++ src/main/java/com/pokemon/pokemon/PokemonController.java
@@ -1,15 +1,27 @@
 package com.pokemon.pokemon;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // Need to use controller because thymeleaf does not support rest controller
 @Controller
 public class PokemonController {
 
-    @GetMapping
-    public String index(){
+    private final PokemonService service;
+
+    @Autowired
+    public PokemonController(PokemonService service){
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String pokemonform(Model model){
+
         return "index";
     }
 }
