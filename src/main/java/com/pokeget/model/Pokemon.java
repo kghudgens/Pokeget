e15261743ffb643ca793a5490033a75c7b6c989d
Kevin Hudgens <hudgens1073@gmail.com>
2022-05-29 19:52:05 +0900

    Create pokemon model

---
+++ src/main/java/com/pokeget/model/Pokemon.java
@@ -0,0 +1,75 @@
+package com.pokeget.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("pokemon")
+public class Pokemon {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String[] type;
+    private String ability;
+    private int height;
+    private int weight;
+
+
+    public Pokemon(String id, String name, String[] type, String ability, int height, int weight) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.ability = ability;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getType() {
+        return type;
+    }
+
+    public void setType(String[] type) {
+        this.type = type;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
