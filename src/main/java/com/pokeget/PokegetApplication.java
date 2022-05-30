61156898204271636af1bb835ccfcc01c5a8d019
Kevin Hudgens <hudgens1073@gmail.com>
2022-05-30 13:25:30 +0900

    autowired the repository

--- src/main/java/com/pokeget/PokegetApplication.java
+++ src/main/java/com/pokeget/PokegetApplication.java
@@ -1,10 +1,18 @@
 package com.pokeget;
 
+import com.pokeget.repository.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class PokegetApplication {
+
+	// autowired the repository so spring boot can find it automatically
+	@Autowired
+	PokemonRepository pokemonRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokegetApplication.class, args);
