2437a452040534fedb521a584f561d734748f91b
Kevin Hudgens <hudgens1073@gmail.com>
2022-05-30 18:00:13 +0900

    removed manual query method

--- src/main/java/com/pokeget/repository/PokemonRepository.java
+++ src/main/java/com/pokeget/repository/PokemonRepository.java
@@ -3,16 +3,10 @@ package com.pokeget.repository;
 import com.pokeget.model.Pokemon;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface PokemonRepository extends MongoRepository<Pokemon, String> {
-
-    @Query("{name:'!0'}")
-    Pokemon findPokemonByName(String name);
-
-
-
-
 
 }
