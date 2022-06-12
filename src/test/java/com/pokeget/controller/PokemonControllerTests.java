fd9b6ea659be1dabebfd2c4eeaea02f37d59c212
Kevin Hudgens <hudgens1073@gmail.com>
2022-06-12 12:34:30 +0900

    Created test for post controller method

--- src/test/java/com/pokeget/controller/PokemonControllerTests.java
+++ src/test/java/com/pokeget/controller/PokemonControllerTests.java
@@ -1,5 +1,6 @@
 package com.pokeget.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pokeget.entity.Pokemon;
 import com.pokeget.repository.PokemonRepository;
 import com.pokeget.service.PokemonService;
@@ -13,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -51,6 +53,35 @@ public class PokemonControllerTests {
                 // the content accessed should be JSON objects
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON)).
                 andDo(print());
+    }
+
+    @Test
+    public void testAddPokemon() throws Exception
+    {
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/pokemon/")
+                .content(asJsonString(mockPokemon))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    /**
+     * Transform the mock pokemon data into a JSON string for testing
+     *
+     * @param mockPokemon data to be turned into the JSON string
+     * @return json string for the test or an error
+     */
+    public static String asJsonString(Pokemon mockPokemon) {
+
+        try{
+            // object mapper provides functionality for reading and writing JSON
+            // serializes the passed in
+            return new ObjectMapper().writeValueAsString(mockPokemon);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
 
