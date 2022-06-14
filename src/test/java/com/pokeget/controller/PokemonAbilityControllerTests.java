531f64837066d557401e4fed180e9e15d966fa35
Kevin Hudgens <hudgens1073@gmail.com>
2022-06-14 18:38:34 +0900

    created mock data and conversion method

--- src/test/java/com/pokeget/controller/PokemonAbilityControllerTests.java
+++ src/test/java/com/pokeget/controller/PokemonAbilityControllerTests.java
@@ -1,11 +1,17 @@
 package com.pokeget.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pokeget.entity.Pokemon;
 import com.pokeget.repository.PokemonRepository;
 import com.pokeget.service.PokemonService;
 import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,7 +21,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,8 +45,34 @@ public class PokemonAbilityControllerTests {
     @MockBean
     private PokemonRepository pokemonRepository;
 
+    @InjectMocks
+    private PokemonAbilityController pokemonAbilityController;
+
+    private List<Pokemon> mockPokemonList;
+
+    Pokemon mockPokemon = new Pokemon("20", "Squirtle", "Water", "", "Torrent", 18, 20, 7);
+    Pokemon mockPokemon2 = new Pokemon("21", "Bulbasaur", "Grass", "", "Overgrow", 2, 15, 1);
+    Pokemon mockPokemon3 = new Pokemon("30", "Feraligtr", "Water", "", "Torrent", 84, 195, 160);
+
+
+    @Before
+    public void setup(){
+        // allows creation of objects required for testing
+        MockitoAnnotations.initMocks(this);
+
+        mockPokemonList = new ArrayList<>();
+        mockPokemonList.add(mockPokemon);
+        mockPokemonList.add(mockPokemon2);
+        mockPokemonList.add(mockPokemon3);
+
+        // when method called, call it against the list
+        when(pokemonRepository.findByAbility("Torrent")).thenReturn(mockPokemonList);
+    }
+
+
+
     @Test
-    public void getPokemonByName() throws Exception {
+    public void getPokemonByAbility() throws Exception {
 
         String url = "/pokemon/ability/{ability}";
         mockMvc.perform(MockMvcRequestBuilders
@@ -42,9 +80,22 @@ public class PokemonAbilityControllerTests {
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                // !figure out how to use json path to test value
-                .andExpect(jsonPath("$.ability").value("Torrent"));
+                .andExpect(MockMvcResultMatchers.content().json(convertObjectToJsonString(mockPokemonList)))
+                .andReturn();
 
+    }
+
+
+
+// fix the object mapper method and test should work
+    private String convertObjectToJsonString(List<Pokemon> mockPokemonList){
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(mockPokemonList);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
     }
 
 
