0c15b1069f3fee43226d345f43009a9097152543
Kevin Hudgens <hudgens1073@gmail.com>
2022-06-16 19:00:48 +0900

    utilized mockmvc to further test controller

--- src/test/java/com/pokeget/controller/PokemonPokedexIDControllerTests.java
+++ src/test/java/com/pokeget/controller/PokemonPokedexIDControllerTests.java
@@ -1,9 +1,12 @@
 package com.pokeget.controller;
 
+import com.pokeget.entity.Pokemon;
 import com.pokeget.repository.PokemonRepository;
 import com.pokeget.service.PokemonService;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,7 +16,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,15 +39,26 @@ public class PokemonPokedexIDControllerTests {
     @MockBean
     private PokemonRepository pokemonRepository;
 
+    @InjectMocks
+    private PokemonPokedexIDController pokemonPokedexIDController;
+
+    Pokemon mockPokemon2 = new Pokemon("21", "Venusaur", "Grass", "Poison", "Overgrow", 72, 220, 3);
+
     @Test
     public void testGetByPokeDexID() throws Exception
     {
+        // data structure acts as repository
+        when(pokemonService.getPokemonByID(3)).thenReturn(List.of(mockPokemon2));
+
         String url = "/pokemon/pokedexID/{pokedexID}";
         mockMvc.perform(MockMvcRequestBuilders
-                        .get(url, 9)
+                        .get(url, 3)
                         .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(1)))
+                        .andExpect(MockMvcResultMatchers.jsonPath("$[0].pokedexID", Matchers.is(3)))
+                        .andExpect(status().isOk())
                         .andDo(print())
-                        .andExpect(status().isOk());
+        ;
 
     }
 
