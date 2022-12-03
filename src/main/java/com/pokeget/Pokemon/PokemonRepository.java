2e571756e5e17af4798c11265236dd237ca30a8a
Kevin Hudgens <hudgens1073@gmail.com>
2022-12-03 13:49:14 -0600

    Corrected the return type for queries

--- src/main/java/com/pokeget/Pokemon/PokemonRepository.java
+++ src/main/java/com/pokeget/Pokemon/PokemonRepository.java
@@ -8,48 +8,18 @@ import java.util.List;
 @Repository
 public interface PokemonRepository extends MongoRepository<PokemonEntity, String> {
 
-    /**
-     * Retrieves the pokemon object by name
-     *
-     * @param name name of the object
-     * @return list of the pokemon
-     */
     @Query("{'name' :?0}")
-    List<PokemonEntity> findPokemonByName(String name);
+    PokemonEntity findPokemonByName(String name);
 
-    /**
-     * Retrieves the pokemon object by its PokeDex id
-     *
-     * @param pokedexID the pokedex id of the pokemon to be deleted
-     * @return list of the pokemon with the pokedex id
-     */
     @Query("{'pokedexID' :?0 }")
-    List<PokemonEntity> findByPokeDexID(int pokedexID);
+    PokemonEntity findByPokeDexID(int pokedexID);
 
-    /**
-     * Retrieves the pokemon object by one of the types listed
-     *
-     * @param type the type of the client is requesting
-     * @return list of the pokemon with the type
-     */
     @Query(" {'$or' : [{'type1' : ?0}, {'type2' : ?0}]}")
     List<PokemonEntity> findByPokemonType(String type);
 
-    /**
-     * Retrieves the pokemon object by its ability
-     *
-     * @param ability name of the ability the user wants to retrieve
-     * @return a list of the pokemon with the ability passed in
-     */
     @Query("{'ability': ?0}")
     List<PokemonEntity> findByAbility(String ability);
 
-    /**
-     * Method retrieves the corresponding pokemon object depending on the id parameter
-     *
-     * @param id unique number created for pokemon object that identifies it
-     * @return the pokemon object with the matching id
-     */
     @Query("{'id': ?0}")
     PokemonEntity findByPokemonID(String id);
 }
